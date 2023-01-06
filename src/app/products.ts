512f171d10b10ef57e9d46d8a6507ae98acd38de
dan.p.wang <dan.p.wang@accenture.com>
2023-01-06 15:56:37 +0900

    Angular フロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,46 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1:"サンプルテキスト1",
+    heading2:"サンプルテキスト2",
+    heading3:"サンプルテキスト3",
+    headingtext1:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1",
+    headingtext2:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト2",
+    headingtext3:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト3"
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1:"サンプルテキスト1",
+    heading2:"サンプルテキスト2",
+    heading3:"サンプルテキスト3",
+    headingtext1:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1",
+    headingtext2:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト2",
+    headingtext3:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト3"
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heading1:"サンプルテキスト1",
+    heading2:"サンプルテキスト2",
+    heading3:"サンプルテキスト3",
+    headingtext1:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1",
+    headingtext2:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト2",
+    headingtext3:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト3"
+  },
+  {
+    name: 'Phone Standard',
+    price: 999,
+    description: '',
+    heading1:"サンプルテキスト1",
+    heading2:"サンプルテキスト2",
+    heading3:"サンプルテキスト3",
+    headingtext1:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト1",
+    headingtext2:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト2",
+    headingtext3:"サンプルテキスト1サンプルテキスト1サンプルテキスト1サンプルテキスト3"
+  }
+];
